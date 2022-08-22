c48e7fb19a4c2d2fddabafdad8a58cab13a408f7
Christian Adrian Bernal <christian.adrian.bernal@gmail.com>
2022-08-22 19:38:40 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-   <h1>La Pelu</h1>
+// function App() {
+//   return (
+//    <h1>La Pelu</h1>
+//   );
+// }
+
+// export default App;
+
+import NavBar from './components/NavBar.js';
+import Curso from './components/Curso';
+import './style/css/style.css';
+
+const App = () => {
+  return(
+    <>
+    <div className='app'>
+    <NavBar />
+      <h1>Coder House</h1>
+      <hr/>
+      {/* esto es un comentario */}
+      <Curso curso="ReactJs" profesor="Leo Messi" duracion="7" precio="500"/>
+      <Curso curso="JavaScript" profesor="Carlos Glarde" duracion="7" precio="100"/>
+    </div>
+    </>
   );
 }
+
 
 export default App;
